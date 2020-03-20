a96dbdaa997b48fafe27e1b68ea5a6ca8cab5cd1
UeharaHua <31815451+UeharaHua@users.noreply.github.com>
2020-03-20 12:14:07 -0400

    added crawler page
    * added crawler page
    
    * update so that axios is only part of the client package
    
    Co-authored-by: BrianGHSchool <bgh4ea@virginia.edu>

---
+++ client/src/components/Crawler/PreviewCard.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import './index.css'
+import { getTheRest } from './igdb.js';
+
+export class PreviewCard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { exported: false };
+        this.exportSQL = this.exportSQL.bind(this);
+    }
+
+    // https://stackoverflow.com/questions/13405129/javascript-create-and-save-file
+    download(data, filename, type) {
+        var file = new Blob([data], { type: type });
+        if (window.navigator.msSaveOrOpenBlob) // IE10+
+            window.navigator.msSaveOrOpenBlob(file, filename);
+        else { // Others
+            var a = document.createElement("a"),
+                url = URL.createObjectURL(file);
+            a.href = url;
+            a.download = filename;
+            document.body.appendChild(a);
+            a.click();
+            setTimeout(function () {
+                document.body.removeChild(a);
+                window.URL.revokeObjectURL(url);
+            }, 0);
+        }
+    }
+
+    multivalueSQL(table, gameID, values) {
+        if (values.length == 0) {
+            return "";
+        }
+        let query = "INSERT INTO " + table + " VALUES ";
+        for (var i = 0; i < values.length; i++) {
+            query += "\n(" + gameID + ", '" + values[i] + "')";
+            query += i < values.length - 1 ? ", " : ";\n\n";
+        }
+        return query;
+    }
+
+    async exportSQL() {
+        let theRest = await getTheRest({ gameID: this.props.details.id });
+        this.setState({ exported: true });
+        if (theRest.success) {
+            let gameQuery = "INSERT INTO Games VALUES (\n'" +
+                this.props.details.id + "', \n'" +
+                this.props.details.name + "', \n'" +
+                theRest.dor.toISOString().slice(0, 19).replace('T', ' ') + "', \n'" +
+                this.props.details.summary + "', \n'" +
+                this.props.details.url + "', \n'" +
+                theRest.bg_img + "', \n'" +
+                this.props.details.coverURL + "', \n'" +
+                theRest.trailer + "');\n\n";
+
+            let genreQuery = this.multivalueSQL("GameGenre", this.props.details.id, theRest.genres);
+            let modeQuery = this.multivalueSQL("GameModes", this.props.details.id, theRest.game_modes);
+            let platformQuery = this.multivalueSQL("GamePlatforms", this.props.details.id, theRest.platforms);
+            let developerQuery = this.multivalueSQL("GameDevelopers", this.props.details.id, theRest.developers);
+            let publisherQuery = this.multivalueSQL("GamePublishers", this.props.details.id, theRest.publishers);
+
+            let allQueries = gameQuery + genreQuery + modeQuery + platformQuery + developerQuery + publisherQuery;
+            this.download(allQueries, './export-test' + this.props.details.id + '.sql', 'text/plain');
+        }
+        else {
+            console.log('Something went wrong.')
+        }
+
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="card gamecard">
+                    <div className="row">
+                        <div className="col-lg-2 col-md-3 col-4">
+                            <img className="game-img" src={this.props.details.coverURL} />
+                        </div>
+                        <div className="col-lg-10 col-md-9 col-8" style={{ marginTop: "10px" }}>
+                            <h2 style={{ wordWrap: "break-word" }}>{this.props.details.name}</h2>
+                            <div className="summary">
+                                {this.props.details.summary}
+                            </div>
+                            <div style={{ justifyContent: "space-between", alignContent: "space-between", display: "flex" }}>
+                                <a href={this.props.details.url}>More information</a>
+                                {this.state.exported
+                                    ? <button className="btn btn-secondary btn-game" style={{ color: "white", marginRight: "10px" }} > Export </button>
+                                    : <button className="btn btn-primary btn-game" style={{ color: "white", marginRight: "10px" }} onClick={() => this.exportSQL()}> Export </button>
+                                }
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <br />
+            </div>
+        );
+    }
+}
---
+++ client/src/components/Crawler/igdb.js
@@ -0,0 +1,270 @@
+import axios from 'axios';
+const USER_KEY = 'fbf8c51fc8298662fc1057f0e26c2399'
+const HEADER = {
+    'Accept': 'application/json',
+    'user-key': USER_KEY
+}
+const DEFAULT_COVER_PATH = "/img/gg-logo.png";
+const PROXY = "https://evening-scrubland-53471.herokuapp.com/";
+const DOMAIN = "https://api-v3.igdb.com/";
+
+// Helper function to make HTTP Requests to IGDB
+async function igdbAPI({ endpoint, data }) {
+    // console.log("Calling " + endpoint + " with " + data)
+    return axios({
+        url: PROXY + DOMAIN + endpoint,
+        method: 'POST',
+        headers: HEADER,
+        data: data
+    });
+}
+
+// cover query
+function coverQuery({ gameID }) {
+    return igdbAPI({
+        endpoint: 'covers',
+        data: 'fields image_id; where game = ' + gameID + ';'
+    });
+}
+
+//gets the first 10 games with matching text
+export async function getPreviews({ searchText }) {
+    let gameData = {};
+    try {
+        const response = await igdbAPI({
+            endpoint: 'games',
+            data: 'search "' + searchText + '"; fields id, name, summary, url; limit 5;'
+        });
+        for (var i in response.data) {
+            gameData[i] = {};
+            gameData[i].id = response.data[i].id;
+            gameData[i].name = response.data[i].name;
+            gameData[i].summary = response.data[i].summary;
+            gameData[i].url = response.data[i].url;
+        }
+        const numGames = response.data.length;
+        let queryArray = [];
+        for (let i_1 = 0; i_1 < numGames; i_1++) {
+            queryArray.push(coverQuery({ gameID: gameData[i_1].id }));
+        }
+        const responseArray = await axios.all(queryArray);
+        for (let i_2 = 0; i_2 < responseArray.length; i_2++) {
+            if (responseArray[i_2].data[0]) {
+                gameData[i_2].coverURL = "https://images.igdb.com/igdb/image/upload/t_cover_big/"
+                    + responseArray[i_2].data[0].image_id + ".jpg";
+            }
+            else {
+                gameData[i_2].coverURL = DEFAULT_COVER_PATH;
+            }
+        }
+        gameData.success = true;
+        return gameData;
+    }
+    catch (err) {
+        console.error(err);
+        return {
+            success: false
+        };
+    }
+}
+
+// retrieves background image url given game id
+async function getBackground({ gameID }) {
+    try {
+        const response = await igdbAPI({
+            endpoint: 'screenshots',
+            data: 'fields image_id; where game = ' + gameID + ';'
+        });
+        console.log("BG ");
+        console.log(response);
+        if (response.data.length == 0){
+            throw "no matching background image data";
+        }
+        return {
+            url: "https://images.igdb.com/igdb/image/upload/t_cover_big/" + response.data[0].image_id + ".jpg",
+            success: true
+        };
+    }
+    catch (err) {
+        console.error(err);
+        return { url: DEFAULT_COVER_PATH, success: false };
+    }
+}
+
+// retrieves first video url given game id
+async function getTrailer({ gameID }) {
+    try {
+        const response = await igdbAPI({
+            endpoint: 'game_videos',
+            data: 'fields video_id; where game = ' + gameID + ';'
+        });
+        if (response.data.length == 0){
+            throw "no matching trailer data";
+        }
+        return {
+            url: "https://www.youtube.com/watch?v=" + response.data[0].video_id,
+            success: true
+        };
+    }
+    catch (err) {
+        console.error(err);
+        return { url: "/", success: false };
+    }
+}
+
+// get metadata not retrieved in preview
+async function getDetails({ gameID }) {
+    let gameData = {};
+    try {
+        const response = await igdbAPI({
+            endpoint: 'games',
+            data: 'where id=' + gameID + '; fields first_release_date, game_modes, genres, involved_companies, platforms;'
+        });
+        if (response.data.length == 0){
+            throw "no matching game data";
+        }
+        gameData.dor = new Date(response.data[0].first_release_date * 1000);
+        gameData.game_modes = '(' + response.data[0].game_modes.toString() + ')';
+        gameData.genres = '(' + response.data[0].genres.toString() + ')';
+        gameData.platforms = '(' + response.data[0].platforms.toString() + ')';
+        gameData.companies = '(' + response.data[0].involved_companies.toString() + ')';
+        gameData.success = true;
+        return gameData;
+    }
+    catch (err) {
+        console.error(err);
+        return {
+            success: false
+        };
+    }
+}
+
+// retrieves genre names given genreIDs
+async function getGenres({ genreIDs }) {
+    let genres = [];
+    try {
+        const response = await igdbAPI({
+            endpoint: 'genres',
+            data: 'where id=' + genreIDs + '; fields name;'
+        });
+        for (var i in response.data) {
+            genres[i] = response.data[i].name;
+        }
+        return genres;
+    }
+    catch (err) {
+        console.error(err);
+        return {
+            success: false
+        };
+    }
+}
+
+// retrieves game modes names given modeIDs
+async function getGameModes({ modeIDs }) {
+    let gameModes = [];
+    try {
+        const response = await igdbAPI({
+            endpoint: 'game_modes',
+            data: 'where id=' + modeIDs + '; fields name;'
+        });
+        for (var i in response.data) {
+            gameModes[i] = response.data[i].name;
+        }
+        return gameModes;
+    }
+    catch (err) {
+        console.error(err);
+        return {
+            success: false
+        };
+    }
+}
+
+// retrieves platform names given platformIDs
+async function getPlatforms({ platformIDs }) {
+    let platforms = [];
+    try {
+        const response = await igdbAPI({
+            endpoint: 'platforms',
+            data: 'where id=' + platformIDs + '; fields name;'
+        });
+        for (var i in response.data) {
+            platforms[i] = response.data[i].name;
+        }
+        return platforms;
+    }
+    catch (err) {
+        console.error(err);
+        return {
+            success: false
+        };
+    }
+}
+
+// retrieves names of developers and publishers given gameID
+async function getCompanies({ gameID }) {
+    let companies = { developers: [], publishers: [] };
+    try {
+        const involvement = await igdbAPI({
+            endpoint: 'involved_companies',
+            data: 'where game=' + gameID + '; fields company, developer, publisher;'
+        });
+        let companyIDs = [];
+        for (var i in involvement.data) {
+            companyIDs.push(involvement.data[i].company);
+        }
+        if (companyIDs.length == 0) {
+            return companies;
+        }
+        const names = await igdbAPI({
+            endpoint: 'companies',
+            data: 'where id=(' + companyIDs.toString() + '); fields name;'
+        });
+        for (var i in names.data) {
+            if (involvement.data[i].developer) {
+                companies.developers.push(names.data[i].name);
+            }
+            if (involvement.data[i].publisher) {
+                companies.publishers.push(names.data[i].name);
+            }
+        }
+        return companies;
+    }
+    catch (err) {
+        console.error(err);
+        return {
+            success: false
+        };
+    }
+}
+
+// get all data not retrieved in preview
+export async function getTheRest({ gameID }) {
+    try {
+        let gameData = await getDetails({ gameID });
+        let companies = await getCompanies({ gameID });
+        gameData.bg_img = await getBackground({ gameID });
+        gameData.bg_img  = gameData.bg_img.url;
+        gameData.trailer = await getTrailer({ gameID });
+        gameData.trailer  = gameData.trailer.url;
+        if (gameData.genres) {
+            gameData.genres = await getGenres({ genreIDs: gameData.genres });
+        }
+        if (gameData.game_modes) {
+            gameData.game_modes = await getGameModes({ modeIDs: gameData.game_modes });
+        }
+        if (gameData.platforms) {
+            gameData.platforms = await getPlatforms({ platformIDs: gameData.platforms });
+        }
+        gameData.developers = companies.developers;
+        gameData.publishers = companies.publishers;
+        delete gameData.companies
+        console.log(gameData);
+        return gameData;
+    }
+    catch (err) {
+        console.error(err);
+        return { success: false };
+    }
+}
