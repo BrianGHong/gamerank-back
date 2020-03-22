6a8816fee99a804f9b9e85146eed9d901ca931b9
Brian Hong <brianhong17@gmail.com>
2020-03-22 16:15:49 -0400

    search routes

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -6,17 +6,48 @@ const database = require("../database");
 
 module.exports = function (pool) {
     
-    router.get("/randomGame", (req, res, next) => {
-
-        // Run query
-        pool.query(`SHOW TABLES;`, (err, data) => {
-            if (err) {
-                res.send(err);
-                throw err;
-            }
-            res.send(data);
-        });        
+    /**
+     * GET Random Game
+     * Returns a random game's ID
+     */
+    router.get("/randomGame", async (req, res) => {
+        await database.query(
+            'SELECT gameID FROM Game', pool
+        ).then((result) => {
+            res.send(result[result.length * Math.random() | 0]);
+        }).catch(err => res.send(err));
     });
+
+    /**
+     * GET Most Favorited Game
+     * Returns an object that contains a game's ID and title
+     */
+    router.get("/mostFavorites", async(req, res) => {
+        await database.query(
+            'SELECT gameID, COUNT(gameID) AS mostFavorite FROM favorites GROUP BY gameID ORDER BY mostFavorite DESC LIMIT 1', pool
+        ).then(result => {
+            return database.query(`SELECT gameID, title FROM Game WHERE gameID=${result[0]["gameID"]}`, pool)
+        }).catch(err => res.send(err))
+        .then(result => {
+            res.send(result[0]);
+        }).catch(err => res.send(err));
+    });
+
+    /**
+     * GET Most Popular Game
+     * Returns an object that contains a game's ID and title
+     */
+    router.get("/mostPopular", async(req, res) => {
+        await database.query(
+            'SELECT gameID, COUNT(gameID) AS mostPopular FROM scores GROUP BY gameID ORDER BY mostPopular DESC LIMIT 1', pool
+        ).then(result => {
+            return database.query(`SELECT gameID, title FROM Game WHERE gameID=${result[0]["gameID"]}`, pool)
+        }).catch(err => res.send(err))
+        .then(result => {
+            res.send(result[0]);
+        }).catch(err => res.send(err));
+    });
+
 
     router.get("/", (req, res) =>{
         res.send("Home Route");
